7c495995fb3e5d9cb3f507bedce6ad86d479961e
McCauley <keaton.mccauley@midway.edu>
2026-08-27 20:17:49 -0400

    2nd attempt task 2

---
+++ Week02/Task2.dart
@@ -0,0 +1,18 @@
+class Student {
+  // fields: name, id, grades
+  String name;
+  int id;
+  List<double> grades ;
+
+  // constructor
+  Student(this.name,this.id, this.grades);
+
+  double calculateGPA() {
+    // return the average of grades
+    return grades.reduce((a, b) => a + b) / grades.length;
+  }
+}
+void main(){
+  Student student = Student("Keaton",100,[100.0, 83.5, 95.2] );
+  print(student.calculateGPA());
+}
